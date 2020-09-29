d0b85a5c00dfc143d7f9d982f195ca9211c9a002
Cody Fritz <clanalia55@gmail.com>
2020-09-29 10:03:20 -0700

    update readme to include capstone proposal

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Computer : MonoBehaviour
@@ -7,21 +9,41 @@ public class Computer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ShowMainMenu();
+        ShowMainMenu("Welcome, User#101");
     }
 
-    void ShowMainMenu() 
+    void ShowMainMenu(string greeting) 
     {
         Terminal.ClearScreen();
-        string greeting = "Hello: Anonymous";
+        // string greeting = "Hello: Anonymous";
         Terminal.WriteLine(greeting);
         Terminal.WriteLine("Welcome to the Mainframe, currently there is '1' game loaded with '3' level(s)");
         Terminal.WriteLine("Press 1, 2, or 3, to play the corresponding level of the loaded game.");
         Terminal.WriteLine("Input your decision here:");
     }
-    // Update is called once per frame
-    void Update()
+
+    void OnUserInput(string input)
     {
-        
+        // Terminal.WriteLine("Message Received: " + input);
+        //print(input == "1"); // Boolean statement, if input==1 then returns True, otherwise, false
+        if(input=="menu")
+        {
+            ShowMainMenu("You have returned to the menu");
+        } else if(input == "1")
+        {
+            print(input == "1");
+                Terminal.WriteLine("You have selected level 1");
+        } else
+        {
+            Terminal.WriteLine("Invalid Entry");
+
+        }
     }
+
+
+    // Update is called once per frame
+   // void Update()
+  //  {
+        
+   // }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Placeholder Readme Header
+Updated Proposal: Unity Platformer 3D - A level, or levels, in unity demonstrating different characteristics and mechanics, MVP will include one fleshed out level as a wireframe/talking point, ideally with jump animations and level triggers. Using Visual Studio and C#, as well as free assets available through Unity and other resources, which will be mentioned in the credits.md as needed.
+  Stretch Goals: Title Screen, Pause Menu, Level Select screen.
+
+Original Proposal: Unity Platformer - A level in unity where you land on a platform and have to jump to get across, stretching across several screens to finish the level. 	C#, Unity	Title Screen, Pause Menu, Restart, A/D Movement, Jump, Respawn when falling off map.
